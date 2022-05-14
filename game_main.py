48b12420bc91e06e8d990d54e6d4e1081bfc0f97
Ronnie <rbeggs10@gmail.com>
2022-05-14 12:56:50 -0700

    Created a specific class for the user spacecraft. Differentiated each planetary body as either stationary or orbiting, with the intent to make the game scalable and to prevent chaotic simulation behavior. Created controls to switch between the main target object. Placed a small triangular marker above every object so they're still visible when zoomed out.

--- game_main.py
+++ game_main.py
@@ -8,10 +8,56 @@ G = 6.67408 * (10**-11)
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (200, 200, 200)
 RED = (255, 000, 000) 
+ORANGE = (255, 165, 000)
 
 
 class Mass:
+
+    def __init__(self, name, mass, radius, stationary, orbital_radius):
+        #characteristics about the mass
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.stationary = stationary
+        self.orbital_radius = orbital_radius
+
+        self.xpos = 0
+        self.ypos = 0
+        self.xvelo = 0
+        self.yvelo = 0
+
+    def __repr__(self):
+        return self.name
+    
+    def gravitational_influence(self, time_interval, barrycenter):
+        deltax = self.xpos - barrycenter.xpos
+        deltay = self.ypos - barrycenter.ypos
+        distance = math.sqrt((deltax**2) + (deltay**2))
+
+        #force of gravity calculated using Newton's Law of Gravition (G defined globally above)
+        force_gravity = (G*self.mass*barrycenter.mass) / (distance**2)
+        #angles drawn from the space craft
+        theta = math.atan2(deltay, deltax)
+
+        net_xforce = force_gravity * -math.cos(theta)
+        net_yforce = force_gravity * -math.sin(theta)
+
+        net_xaccel = net_xforce / self.mass
+        net_yaccel = net_yforce / self.mass
+
+        #calculates the change in velocity over a given time step interval
+        self.xvelo += net_xaccel * time_interval
+        self.yvelo += net_yaccel * time_interval	
+
+    def deltaPosition(self, time_interval):
+        #determines the change in position over the same time step interval
+        self.xpos += self.xvelo * time_interval
+        self.ypos += self.yvelo * time_interval
+
+
+class SpaceCraft:
 
     def __init__(self, name, mass, radius, init_xpos=0, init_ypos=0, init_xvelo=0, init_yvelo=0):
         self.name = name
@@ -30,15 +76,13 @@ class Mass:
         #all angles drawn from the space craft
         gravity_vectors = []
 
-        for planet in planet_list:
-            if planet == self:
-                continue #prevents planets from calculating gravity in relation to themselves
-            deltax = self.xpos - planet.xpos
-            deltay = self.ypos - planet.ypos
+        for body in body_list:
+            deltax = self.xpos - body.xpos
+            deltay = self.ypos - body.ypos
             distance = math.sqrt((deltax**2) + (deltay**2))
 
             #force of gravity calculated using Newton's Law of Gravition (G defined globally above)
-            force_gravity = (G*self.mass*planet.mass) / (distance**2)
+            force_gravity = (G*self.mass*body.mass) / (distance**2)
             theta = math.atan2(deltay, deltax)
 
             gravity_vectors.append([force_gravity, theta])
@@ -63,6 +107,7 @@ class Mass:
         self.ypos += self.yvelo * time_interval
 
 
+
 def scale_converter(real_distance, SCALE_FACTOR):
 
     scaled_distance = real_distance / SCALE_FACTOR
@@ -70,12 +115,13 @@ def scale_converter(real_distance, SCALE_FACTOR):
 
 
 #initialize each planetary body
-#name, mass, radius, init_xpos=0, init_ypos=0, init_xvelo=0, init_yvelo=0
-kerbol = Mass("kerbol", 5.972*(10**24), 6378, 0, 0, 0, 0)
+#name, mass, radius, stationary, orbital_radius
+earth = Mass("Earth", 5.97*(10**24), 6378, False, 149.6*(10**6))
+sun = Mass("Sun", 1.99*(10**30), 6.96*(10**5), True, 0)
 
-planet_list = [kerbol]
+body_list = (earth, sun)
 
-starship = Mass("starship", 10, 1, 179828, 0, 0, -47078)
+starship = SpaceCraft("starship", 10, 1, (149.6*(10**6)+35785), 0, 0, 3070)
 
 
 
@@ -87,7 +133,27 @@ def main(screen_width, screen_height, simulation_width, iteration_pause):
     REAL_WIDTH = simulation_width #raw width of the display in km
     SCALE_FACTOR = round(REAL_WIDTH / DP_WIDTH) #ratio of km:1 pixel
     TARGET = starship #either a class object or x-y coordinates
+
+    #starship will always have the 0 index
+    target_list = [starship]
+    for body in body_list:
+        target_list.append(body)
     
+    barrycenter = None
+    for body in body_list:
+        if body.stationary == True:
+            barrycenter = body
+            body.xpos, body.ypos = 0, 0
+            body.xvelo, body.xvelo = 0, 0
+            break
+    
+    for body in body_list:
+        if body != barrycenter:
+            body.xpos = body.orbital_radius + barrycenter.xpos
+            body.ypos = barrycenter.ypos
+            body.xvelo = 0
+            body.yvelo = math.sqrt(G*barrycenter.mass*(1/body.orbital_radius)) 
+
     #initializes pygame library tools and display window
     pygame.init()
     screen = pygame.display.set_mode((DP_WIDTH, DP_HEIGHT))
@@ -109,15 +175,27 @@ def main(screen_width, screen_height, simulation_width, iteration_pause):
             if event.type == pygame.KEYDOWN:
                 
                 if event.key == pygame.K_1:
-                    SCALE_FACTOR = round(0.9 * SCALE_FACTOR)
+                    SCALE_FACTOR = round(0.5 * SCALE_FACTOR)
                 if event.key == pygame.K_2:
-                    SCALE_FACTOR = round(1.1 * SCALE_FACTOR)
+                    SCALE_FACTOR = round(2 * SCALE_FACTOR)
+
+                if event.key == pygame.K_q:
+                    if target_list.index(TARGET)+1 == len(target_list):
+                        TARGET = target_list[0]
+                    else:
+                        TARGET = target_list[target_list.index(TARGET)+1]
+                if event.key == pygame.K_e:
+                    if target_list.index(TARGET)+1 == len(target_list):
+                        TARGET = target_list[0]
+                    else:
+                        TARGET = target_list[target_list.index(TARGET)-1]   
 
 
         #calculate gravity's influence on each orbiting body
-        for planet in planet_list:
-            planet.gravitational_influence(0.01)
-            planet.deltaPosition(0.01)
+        for body in body_list:
+            if body != barrycenter:
+                body.gravitational_influence(0.01, barrycenter)
+                body.deltaPosition(0.01)
 
         starship.gravitational_influence(0.01)
         starship.deltaPosition(0.01)
@@ -127,20 +205,22 @@ def main(screen_width, screen_height, simulation_width, iteration_pause):
 
         screen.fill(BLACK)
         #draw planets
-        for planet in planet_list:
-            planet_dp_position = [DP_CENTER_X + scale_converter(planet.xpos, SCALE_FACTOR), DP_CENTER_Y + scale_converter(planet.ypos, SCALE_FACTOR)]
+        for body in body_list:
+            dp_position = [DP_CENTER_X + scale_converter(body.xpos, SCALE_FACTOR), DP_CENTER_Y + scale_converter(body.ypos, SCALE_FACTOR)]
             #draw.circle(surface, color, center, radius)
-            pygame.draw.circle(screen, WHITE, planet_dp_position, scale_converter(planet.radius, SCALE_FACTOR))
-
+            pygame.draw.circle(screen, WHITE, dp_position, scale_converter(body.radius, SCALE_FACTOR))
+            pygame.draw.polygon(screen, ORANGE, [dp_position, [dp_position[0]+5, dp_position[1]-10], [dp_position[0]-5, dp_position[1]-10]])
+        
         #draw craft
         craft_dp_position = [DP_CENTER_X + scale_converter(starship.xpos, SCALE_FACTOR), DP_CENTER_Y + scale_converter(starship.ypos, SCALE_FACTOR)]
         #using an inverse tangent function allows the starship to scale according to two asymptotes
         craft_dp_radius = -20*math.atan(0.1*SCALE_FACTOR - 12) + 35
-        pygame.draw.circle(screen, RED, craft_dp_position, craft_dp_radius)
+        pygame.draw.circle(screen, GREY, craft_dp_position, starship.radius)
+        #pygame.draw.polygon(screen, RED, [craft_dp_position, [craft_dp_position[0]+5, craft_dp_position[1]-10], [craft_dp_position[0]-5, craft_dp_position[1]-10]])
         
         pygame.display.set_caption(str(i))
         pygame.display.update()
         time.sleep(iteration_pause)
 
 #parameters: real width included in the display window, iteration pause interval
-main(1000, 1000, 400000, 0.01)
+main(1000, 1000, 100000, 0.01)

c2be2b7d87d09d77a31baf29b901da0a7f0ccca4
Ronnie Beggs <rbeggs@berkeley.edu>
2024-09-01 13:51:53 -0700

    [cleanup] remove unused files.

--- engine3d/src/Main.java
+++
@@ -1,9 +0,0 @@
-import engine.Engine;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starting Engine");
-        Engine engine = new Engine();
-        engine.mainLoop();
-    }
-}
